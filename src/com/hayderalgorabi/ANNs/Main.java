b4187885c46b45510f6166bbca7b7d43c4f9889d
Hayder J. Kadhim <haeder.algorabi@gmail.com>
2017-06-02 18:37:16 +0300

    Update Main.java

--- src/com/hayderalgorabi/ANNs/Main.java
+++ src/com/hayderalgorabi/ANNs/Main.java
@@ -4,12 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            int InputNodes = 2;
-            int HiddenNodes = 12;
+            Encoder en = new Encoder();
+            try {
+                en.SetInputDataPath("e:/in2.data");
+                en.SetOutputDataPath("e:/out.csv");
+                en.BuildDataset();
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+
+            int InputNodes = 32;
+            int HiddenNodes = 60;
             double Error = 0.01;
-            int Itertaion = 500;
-            String PatternsPath = "e:/Patterns.csv";
-            new Network(InputNodes,HiddenNodes,Error,Itertaion,PatternsPath);
+            int Itertaion = 700000;
+            String PatternsPath = "e:/out.csv";
+            new Network(InputNodes,HiddenNodes,Error,Itertaion,PatternsPath,en);
 
         } catch (Exception e) {
             e.printStackTrace();
